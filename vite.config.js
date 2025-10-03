e376e6ad9a203d0f2a652592694ab7710e0eda8d
n3vermin9 <89288662119s@mail.ru>
2025-10-03 10:50:01 +0300

    minor changes

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import os from 'os';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import os from "os";
 
 function getLocalIp() {
   const interfaces = os.networkInterfaces();
   for (const name in interfaces) {
     for (const iface of interfaces[name]) {
-      if (iface.family === 'IPv4' && !iface.internal) {
+      if (iface.family === "IPv4" && !iface.internal) {
         return iface.address;
       }
     }
   }
-  return '127.0.0.1';
+  return "127.0.0.1";
 }
 
 export default defineConfig({
   plugins: [react()],
+  base: "/iShop/",
   server: {
-    host: true, // allows network access
+    host: true,
     port: 5173,
-    // Hook into server start
     configureServer(server) {
-      server.httpServer?.once('listening', () => {
+      server.httpServer?.once("listening", () => {
         const ip = getLocalIp();
         console.log(`\n  Local IP: http://${ip}:5173\n`);
       });
-    }
-  }
+    },
+  },
 });
